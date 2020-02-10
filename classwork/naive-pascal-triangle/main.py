b993dc5f34dd63b6e43a1b1c574d91ab12ddb436
rhdzmota <rhdzmota@gmail.com>
2020-02-10 17:44:13 -0600

    CR-23 Fix type annotations!

--- classwork/naive-pascal-triangle/main.py
+++ classwork/naive-pascal-triangle/main.py
@@ -8,14 +8,14 @@ logger = logging.getLogger(__name__)
 
 class Main(object):
 
-    def _get_element(self, i, j):
+    def _get_element(self, i: int, j: int):
         pass
 
-    def _naive_implementation(self, level, index=0):
+    def _naive_implementation(self, level: int, index: int = 0):
         print("Missing implementation!")
 
     @timeit(logger)
-    def pascal_triangle(self, level: str, start: int = 0):
+    def pascal_triangle(self, level: int, start: int = 0):
         self._naive_implementation(level, index=start)
 
 
