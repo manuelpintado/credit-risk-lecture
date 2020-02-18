9a7d8877a2813d62c57647af5c50b708d3d3c575
manuelpintado <if707405@iteso.mx>
2020-02-17 19:38:21 -0600

    Optimizacion de pascal implementation

--- classwork/optimized-pascal-triangle/main.py
+++ classwork/optimized-pascal-triangle/main.py
@@ -1,22 +1,36 @@
 import fire
 import logging
 
-from utils import timeit
+from utils import timeit, TriangleBuilder, caching
 
 logger = logging.getLogger(__name__)
 
 
 class Main(object):
-
+    _builder = TriangleBuilder()
+    @caching
     def _get_element(self, i: int, j: int):
-        pass
+        return 1 if (j >= i or j == 0) else \
+            self._get_element(i=i - 1, j=j - 1) + self._get_element(i=i - 1, j=j)
 
     def _naive_implementation(self, level: int, index: int = 0):
-        print("Missing implementation!")
+        if index < level:
+            row = [str(self._get_element(i=index, j=j)) for j in range(index + 1)]
+            print(" ".join(row))
+            self._naive_implementation(level=level, index=index + 1)
+
+    def _optimized_implementation(self, level: int, index: int = 0):
+        if index < level:
+            row = self._builder.get_row(index=index)
+            print(" ".join(row))
+            self._optimized_implementation(level=level, index=index + 1)
 
     @timeit(logger)
-    def pascal_triangle(self, level: int, start: int = 0):
-        self._naive_implementation(level, index=start)
+    def pascal_triangle(self, level: int, option: str, start: int = 0):
+        if "naive" == option.lower():
+            self._naive_implementation(level, index=start)
+        if "optimized" == option.lower():
+            self._optimized_implementation(level, index=start)
 
 
 if __name__ == "__main__":
