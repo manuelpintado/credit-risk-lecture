53d77b693b2651f7054e06bb78fde5ad0b576e1a
manuelpintado <if707405@iteso.mx>
2020-03-08 18:53:21 -0600

    examen 1

--- exam/flatten-json/utils.py
+++ exam/flatten-json/utils.py
@@ -12,6 +12,44 @@ def pretty_print(logger, serializer_function=lambda obj: obj.__dict__):
                 print(formatted_output)
             except TypeError as e:
                 logger.error("Type Error encounter with message {error}".format(error=e))
-                raise  # Re-throw exception to fail the program execution with stack-trace.
+                raise
+
         return wrapper
+
     return decorator
+
+
+def load_file(filename):
+    with open(filename, "r") as file:
+        data = json.loads(file.read())
+    return data
+
+
+def flatten_dict(d, parent_key='', separator='.'):
+    items = []
+    index = 0
+    if isinstance(d, dict):
+        for n, (k, v) in enumerate(d.items()):
+            new_key = parent_key + str(separator) + k if parent_key else k
+            if isinstance(v, dict):
+                items.extend(flatten_dict(v, new_key, separator=separator).items())
+            if isinstance(v, list):
+                items.extend(flatten_dict(v, new_key, separator=separator).items())
+                index += 1
+            else:
+                items.append((new_key, v))
+    elif isinstance(d, list):
+        for k, v in enumerate(d):
+            new_key = parent_key + str(separator) + str(k) if parent_key else k
+            if isinstance(v, dict):
+                items.extend(flatten_dict(v, new_key, separator=separator).items())
+            if isinstance(v, list):
+                items.extend(flatten_dict(v, new_key, separator=separator).items())
+                index += 1
+            else:
+                items.append((new_key, v))
+    return dict(items)
+
+
+def flatten(dictionary):
+    return {k: v for k, v in dictionary.items() if not isinstance(v, dict or list)}
