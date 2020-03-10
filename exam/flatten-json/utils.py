b008ceab1f9662773fe498d472464f13d90c087d
manuelpintado <if707405@iteso.mx>
2020-03-09 19:19:05 -0600

    Examen resualto correctamente

--- exam/flatten-json/utils.py
+++ exam/flatten-json/utils.py
@@ -15,3 +15,22 @@ def pretty_print(logger, serializer_function=lambda obj: obj.__dict__):
                 raise  # Re-throw exception to fail the program execution with stack-trace.
         return wrapper
     return decorator
+
+
+def replace_list_elements(obj):
+    if isinstance(obj, list):
+        return {str(i): replace_list_elements(v) for i, v in enumerate(obj)}
+    if isinstance(obj, dict):
+        return {k: replace_list_elements(v) for k, v in obj.items()}
+    return obj
+
+
+def flatten_dict(dictionary, sep=".", prefix=""):
+    flat_dict = {}
+    for key, val in dictionary.items():
+        flat_key = "{prefix}{sep}{key}".format(prefix=prefix, sep=sep, key=key) if prefix else key
+        flat_dict = {
+            **flat_dict,
+            **(flatten_dict(dictionary=val, sep=sep, prefix=flat_key) if isinstance(val, dict) else {flat_key: val})
+        }
+    return flat_dict
